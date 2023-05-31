a9e65ea416db060f88a13a2786a6d7142bd8f453
Luc <luc@lucemans.nl>
2023-05-31 13:02:38 +0000

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{Read, Write};
+use std::{
+    env::temp_dir,
+    os::{fd::FromRawFd, unix::net::UnixListener},
+    path::Path,
+};
+
+use nix::{fcntl::OFlag, sys::stat::Mode, unistd::mkfifo};
+
 fn main() {
-    println!("Hello, world!");
+    
 }
