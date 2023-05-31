d017458cc03997ec223fc0a00853d19a5bb9bc20
Luc <luc@lucemans.nl>
2023-05-31 13:06:04 +0000

    Bump README

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## TLDR;
+It decodes the funny text whisper.cpp outputs over stream to other format.
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::io::{Read, Write};
-use std::{
-    env::temp_dir,
-    os::{fd::FromRawFd, unix::net::UnixListener},
-    path::Path,
-};
-
-use nix::{fcntl::OFlag, sys::stat::Mode, unistd::mkfifo};
-
 fn main() {
     
 }
